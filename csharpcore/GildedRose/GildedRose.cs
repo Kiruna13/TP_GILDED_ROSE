2160cb68a0853956c97c04ae609e16446c47c979
Thomas Ducoulombier <thomas.ducoulombier@free.fr>
2022-10-28 09:29:33 +0200

    add tests and conjured items

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -2,6 +2,6 @@ namespace GildedRoseKata
 {
     public class GildedRose
     {
-
+       
     }
 }
--- csharpcore/GildedRose/Item.cs
+++ csharpcore/GildedRose/Item.cs
@@ -1,74 +1,81 @@
+using System;
+
 namespace GildedRoseKata
 {
-	/// <summary>
-	/// class item
-	/// </summary>
-	abstract class Item
-	{
-		protected string name;
-		protected int sellIn;
-		protected int quality;
-		public Item(string name, int sellIn, int quality)
-		{
-			this.name = name;
-			this.sellIn = sellIn;
-			this.quality = quality;
-		}
+    public abstract class Item
+    {
+        public string name {get; protected set;}
+        public int sellIn {get; protected set;}
+        public int quality {get; protected set;}
 
-		public abstract void update();
+        public Item(string name, int sellIn, int quality){
+            this.name = name;
+            this.sellIn = sellIn;
+            this.quality = quality;
+        }
 
-		// public void updateQuality(){
-			
-		// 	switch(name)
-		// 	{
-		// 		case "Aged Brie":
-		// 			quality ++;
-		// 			break;
-		// 		case "Backstage passes":
-				
-		// 			switch (sellIn)
-        //             {
-        //                 case <= 0:
-        //                     quality = 0;
-        //                     break;
-        //                 case <= 5:
-        //                     quality += 3;
-        //                     break;
-        //                 case <= 10:
-        //                     quality += 2;
-        //                     break;
-        //                 default:
-        //                     quality++;
-        //                     break;
-        //             }
-		// 			break;
-		// 		default:
-		// 			if(sellIn > 0)
-		// 			{
-		// 				sellIn--;
-		// 				quality--;
-		// 			}
-		// 			else
-		// 			{
-		// 				quality = quality-2;
-		// 			}
-		// 			break;
-		// 	}
-			
+        public abstract void Update();
+        // {
+        //     if(this.name == "Sulfuras")
+        //         return;
 
-			
-		// }
-		// public void checkQuality()
-		// {
-		// 	if(quality < 0){
-		// 		quality = 0;
-		// 	}
-		// 	if(quality > 50){
-		// 		quality = 50;
-		// 	}
-		// 	if(name == "sulfuras"){
-		// 		quality = 80;
-		// 	}
-		// }
-	}
+        //     UpdateSellIn();
+        //     UpdateQuality();
+        // }
+
+        // public void UpdateSellIn(){
+            
+        //     this.sellIn--;
+        // }
+
+        // public void UpdateQuality(){
+        //     switch(this.name)
+        //     {
+        //         case "Aged Brie": UpdateAgingItemQuality(); break;
+        //         case "Backstage Pass": UpdateBackstagePassQuality(); break;
+        //         default: UpdateGenericItemQuality(); break;
+        //     }
+
+        //     FloorQualityToZero();
+        //     CeilQualityToFifty();
+        // }
+
+        // private void UpdateBackstagePassQuality()
+        // {
+        //     this.quality++;
+
+        //     if(this.sellIn <= 10)
+        //         this.quality++;
+
+        //     if(this.sellIn <= 5)
+        //         this.quality++;
+
+        //     if(this.sellIn < 0)
+        //         this.quality = 0;
+        // }
+
+        // private void UpdateGenericItemQuality()
+        // {
+        //     if (this.sellIn < 0)
+        //         this.quality--;
+
+        //     this.quality--;
+        // }
+
+        // private void UpdateAgingItemQuality()
+        // {
+        //     this.quality++;
+        // }
+
+        protected void FloorQualityToZero(){
+            if(this.quality < 0)
+                this.quality = 0;
+        }
+        protected void CeilQualityToFifty(){
+            if(this.quality > 50)
+                this.quality = 50;
+        }
+
+        
+    }
 }
